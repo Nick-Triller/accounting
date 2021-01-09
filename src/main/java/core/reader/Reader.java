e7e7048f9eb23c0ee95fefa3f125ba203c88c8b8
Mark Bucciarelli <mkbucc@gmail.com>
2021-01-09 09:48:18 -0500

    Conform to project code style, no logic change
    Changes:
      * four space indent (instead of two)
      * eight-space indent for continuation lines
      * no blank lines in methods

--- src/main/java/core/reader/Reader.java
+++ src/main/java/core/reader/Reader.java
@@ -6,5 +6,5 @@ import core.Ledger;
  A Reader can read double-entries from a persistent store.
  */
 public interface Reader {
-  Ledger read();
+    Ledger read();
 }
--- src/main/java/core/reader/TabDelimitedReader.java
+++ src/main/java/core/reader/TabDelimitedReader.java
@@ -30,174 +30,151 @@ import lombok.Value;
  from a tab-delimited file.
 
  <p>
-   See sample files in test/resources for examples.
-   Skips blanks lines and lines that start with a '#'.
+ See sample files in test/resources for examples.
+ Skips blanks lines and lines that start with a '#'.
  </p>
 
  */
 @Value
 public class TabDelimitedReader implements Reader {
+    public static final String DELIMITER = "\\t";
+    String fileName;
 
-  public static final String DELIMITER = "\\t";
-
-  String fileName;
-
-
-  @Override
-  public Ledger read() {
-
-    Set<AccountDetails> accountDetails = new HashSet<>();
-    List<AccountingTransaction> transactions = new ArrayList<>();
-
-    try (Stream<String> rows = Files.lines(Path.of(fileName))) {
-      rows
-          .map(row -> row.split(DELIMITER))
-          .forEach(x -> processRow(x, accountDetails, transactions));
-    } catch (IOException e) {
-      throw new IllegalStateException("can't read '" + fileName + "'", e);
+    @Override
+    public Ledger read() {
+        Set<AccountDetails> accountDetails = new HashSet<>();
+        List<AccountingTransaction> transactions = new ArrayList<>();
+        try (Stream<String> rows = Files.lines(Path.of(fileName))) {
+            rows
+                    .map(row -> row.split(DELIMITER))
+                    .forEach(x -> processRow(x, accountDetails, transactions));
+        } catch (IOException e) {
+            throw new IllegalStateException("can't read '" + fileName + "'", e);
+        }
+        Journal journal = new Journal();
+        transactions.forEach(journal::addTransaction);
+        return new Ledger(
+                journal,
+                new ChartOfAccounts(Set.copyOf(accountDetails))
+        );
     }
 
-    Journal journal = new Journal();
-    transactions.forEach(journal::addTransaction);
-    return new Ledger(
-        journal,
-        new ChartOfAccounts(Set.copyOf(accountDetails))
-    );
-
-  }
-
-
-  // Convert row to either an AccountDetail or AccountingTransaction.
-  private void processRow(
-      String[] columns,
-      Set<AccountDetails> accountDetails,
-      List<AccountingTransaction> transactions) {
-
-    // Skip blank lines.
-    if (columns.length == 0 || columns[0].isBlank()) {
-      return;
+    // Convert row to either an AccountDetail or AccountingTransaction.
+    private void processRow(
+            String[] columns,
+            Set<AccountDetails> accountDetails,
+            List<AccountingTransaction> transactions) {
+        // Skip blank lines.
+        if (columns.length == 0 || columns[0].isBlank()) {
+            return;
+        }
+        // Skip lines that are comments.
+        if (columns[0].trim().startsWith("#")) {
+            return;
+        }
+        // Skip header line.
+        if (columns[0].trim().equalsIgnoreCase("entry_type")) {
+            return;
+        }
+        EventType eventType = null;
+        try {
+            eventType = EventType.valueOf(columns[0]);
+        } catch (Exception e) {
+            validationErr("invalid event type", e, columns);
+        }
+        switch (eventType) {
+            case ACCOUNT:
+                accountDetails.add(rowToAccount(columns));
+                break;
+            case TRANSACTION:
+                transactions.add(rowToTransaction(columns));
+                break;
+            default:
+                logicErr(columns);
+        }
     }
 
-    // Skip lines that are comments.
-    if (columns[0].trim().startsWith("#")) {
-      return;
+    private AccountDetails rowToAccount(String[] columns) {
+        String number = columns[1].trim();
+        String name = columns[2].trim();
+        String increasesOn = columns[3].trim();
+        AccountDetails y = null;
+        try {
+            y = new AccountDetailsImpl(
+                    number,
+                    name,
+                    AccountSide.valueOf(increasesOn));
+        } catch (Exception e) {
+            validationErr("invalid account data", e, columns);
+        }
+        return y;
     }
 
-    // Skip header line.
-    if (columns[0].trim().equalsIgnoreCase("entry_type")) {
-      return;
+    private AccountingTransaction rowToTransaction(String[] columns) {
+        String dateTime = columns[4].trim();
+        String debitAccounts = columns[5].trim();
+        String debitAmounts = columns[6].trim();
+        String creditAccounts = columns[7].trim();
+        String creditAmounts = columns[8].trim();
+        Set<AccountingEntry> entries = toAccountingEntryset(
+                DEBIT,
+                debitAccounts,
+                debitAmounts);
+        entries.addAll(toAccountingEntryset(
+                CREDIT,
+                creditAccounts,
+                creditAmounts));
+        // 2011-12-03T10:15:30+01:00
+        long transactionTime = ZonedDateTime
+                .parse(dateTime, ISO_OFFSET_DATE_TIME)
+                .toInstant()
+                .toEpochMilli();
+        AccountingTransaction y = null;
+        try {
+            y = new AccountingTransaction(
+                    entries,
+                    null,
+                    transactionTime);
+        } catch (Exception e) {
+            validationErr("invalid transaction data", e, columns);
+        }
+        return y;
     }
 
-    EventType eventType = null;
-    try {
-      eventType = EventType.valueOf(columns[0]);
-    } catch (Exception e) {
-      validationErr("invalid event type", e, columns);
+    private Set<AccountingEntry> toAccountingEntryset(
+            AccountSide accountSide,
+            String accounts,
+            String amounts
+    ) {
+        Set<AccountingEntry> ys = new HashSet<>();
+        String[] as = accounts.split(",");
+        String[] bs = amounts.split(",");
+        for (int i = 0; i < as.length; i++) {
+            ys.add(
+                    // BigDecimal amount, String accountNumber, AccountSide accountSide
+                    new AccountingEntry(
+                            new BigDecimal(bs[i]),
+                            as[i],
+                            accountSide
+                    )
+            );
+        }
+        return ys;
     }
 
-    switch (eventType) {
-      case ACCOUNT:
-        accountDetails.add(rowToAccount(columns));
-        break;
-      case TRANSACTION:
-        transactions.add(rowToTransaction(columns));
-        break;
-      default:
-        logicErr(columns);
+    // Convenience function for raising validate errors.
+    private String errMessage(String msg, String[] columns) {
+        return msg + " on line with columns: " + Arrays.toString(columns);
     }
 
-  }
-
-  private AccountDetails rowToAccount(String[] columns) {
-    String number = columns[1].trim();
-    String name = columns[2].trim();
-    String increasesOn = columns[3].trim();
-    AccountDetails y = null;
-    try {
-      y = new AccountDetailsImpl(
-              number,
-              name,
-              AccountSide.valueOf(increasesOn));
-    } catch (Exception e) {
-      validationErr("invalid account data", e, columns);
-    }
-    return y;
-  }
-
-  private AccountingTransaction rowToTransaction(String[] columns) {
-    String dateTime = columns[4].trim();
-    String debitAccounts = columns[5].trim();
-    String debitAmounts = columns[6].trim();
-    String creditAccounts = columns[7].trim();
-    String creditAmounts = columns[8].trim();
-
-
-    Set<AccountingEntry> entries = toAccountingEntryset(
-        DEBIT,
-        debitAccounts,
-        debitAmounts);
-    entries.addAll(toAccountingEntryset(
-        CREDIT,
-        creditAccounts,
-        creditAmounts));
-
-    // 2011-12-03T10:15:30+01:00
-    long transactionTime = ZonedDateTime
-        .parse(dateTime, ISO_OFFSET_DATE_TIME)
-        .toInstant()
-        .toEpochMilli();
-
-    AccountingTransaction y = null;
-    try {
-      y = new AccountingTransaction(
-              entries,
-              null,
-              transactionTime);
-    } catch (Exception e) {
-      validationErr("invalid transaction data", e, columns);
-    }
-    return y;
-  }
-
-  private Set<AccountingEntry> toAccountingEntryset(
-      AccountSide accountSide,
-      String accounts,
-      String amounts
-  ) {
-
-    Set<AccountingEntry> ys = new HashSet<>();
-    String[] as = accounts.split(",");
-    String[] bs = amounts.split(",");
-    for (int i = 0; i < as.length; i++) {
-      ys.add(
-          // BigDecimal amount, String accountNumber, AccountSide accountSide
-          new AccountingEntry(
-              new BigDecimal(bs[i]),
-              as[i],
-              accountSide
-          )
-      );
+    private void logicErr(String[] columns) {
+        throw new IllegalStateException(
+                errMessage("logic error in code", columns));
     }
 
-    return ys;
+    private void validationErr(String msg, Throwable e, String[] columns) {
+        throw new IllegalStateException(errMessage(msg, columns), e);
+    }
 
-  }
-
-  // Convenience function for raising validate errors.
-  private String errMessage(String msg, String[] columns) {
-    return msg + " on line with columns: " + Arrays.toString(columns);
-  }
-
-  private void logicErr(String[] columns) {
-    throw new IllegalStateException(
-        errMessage("logic error in code", columns));
-  }
-
-  private void validationErr(String msg, Throwable e, String[] columns) {
-    throw new IllegalStateException(errMessage(msg, columns), e);
-  }
-
-
-  public enum EventType {ACCOUNT, TRANSACTION}
-
+    public enum EventType {ACCOUNT, TRANSACTION}
 }
